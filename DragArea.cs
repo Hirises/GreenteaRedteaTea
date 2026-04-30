ea6a116a09e2a44175f23307c5df81fb4d5d14f5
PulleyBun <z22x22c22@naver.com>
2026-05-01 03:49:22 +0900

    기본적인 드래그 인식

---
+++ DragArea.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class DragArea : Area2D
+{
+    public void OnArea2DEntered()
+    {
+        InputManager.Instance?.OnAreaEntered(this);
+    }
+
+    public void OnArea2DExited()
+    {
+        InputManager.Instance?.OnAreaExited(this);
+    }
+}
---
+++ InputManager.cs
@@ -0,0 +1,94 @@
+using Godot;
+using System;
+
+public partial class InputManager : Node
+{
+    public static InputManager Instance { get; private set; }
+
+    public enum InputState
+    {
+        None,
+        MouseDown,
+        Dragging,
+    }
+
+    InputState inputState = InputState.None;
+    DragArea lastClickArea;
+    DragArea currentHoverArea;
+
+    Vector2 lastClickPosition;
+    [Export] float dragThreshold = 10f;
+
+    public override void _Ready()
+    {
+        if (Instance != null)
+        {
+            GD.PrintErr("Multiple instances of InputManager detected!");
+            QueueFree();
+            return;
+        }
+        Instance = this;
+    }
+
+    public void OnAreaEntered(DragArea area)
+    {
+        currentHoverArea = area;
+    }
+
+    public void OnAreaExited(DragArea area)
+    {
+        if (currentHoverArea == area)
+        {
+            currentHoverArea = null;
+        }
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseEvent)
+        {
+            if (mouseEvent.Pressed)
+            {
+                lastClickPosition = mouseEvent.Position;
+                lastClickArea = currentHoverArea;
+                inputState = InputState.MouseDown;
+            }
+            else
+            {
+                if (inputState == InputState.Dragging)
+                {
+                    OnDragEnd(currentHoverArea);
+                }
+                else if (inputState == InputState.MouseDown)
+                {
+                    OnClick(lastClickArea);
+                }
+                inputState = InputState.None;
+            }
+        }
+        else if (@event is InputEventMouseMotion motionEvent)
+        {
+            if (inputState == InputState.MouseDown)
+            {
+                if (motionEvent.Position.DistanceTo(lastClickPosition) > dragThreshold)
+                {
+                    inputState = InputState.Dragging;
+                    OnDragStart(lastClickArea);
+                }
+            }
+        }
+    }
+
+    void OnClick(DragArea area)
+    {
+        GD.Print($"Clicked on {area?.Name}");
+    }
+    void OnDragStart(DragArea area)
+    {
+        GD.Print($"Started dragging on {area?.Name}");
+    }
+    void OnDragEnd(DragArea area)
+    {
+        GD.Print($"Ended dragging on {area?.Name}");
+    }
+}
