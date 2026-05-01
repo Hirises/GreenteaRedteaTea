adf8dde3e76e9d0e872b07a08acc918d4c4320f5
PulleyBun <z22x22c22@naver.com>
2026-05-01 21:05:11 +0900

    말풍선 애니메이션

--- Scripts/Game/UI/CustomerUi.cs
+++ Scripts/Game/UI/CustomerUi.cs
@@ -10,7 +10,6 @@ public partial class CustomerUi : Sprite2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		ZIndex = 0;
 		FindTextLabel();
 	}
 
