2a1ef9c90834a44963a7c621a6b0a2ce5e44ed0c
allcho <allchotv@gmail.com>
2026-05-01 23:49:57 +0900

    후반용 랜덤 손님

---
+++ Scripts/Game/Customers/RandomCustomer.cs
@@ -0,0 +1,21 @@
+using RedteaGreenteaTea.Domain;
+
+public class RandomCustomer : Customer
+{
+    private const float DefaultPatienceSeconds = 30f;
+    private const int DefaultOrderMaxDepth = 2;
+
+    public RandomCustomer(int number) : base(number)
+    {
+        PatienceSeconds = DefaultPatienceSeconds;
+    }
+
+    protected override ProductExpression _GenerateOrder()
+    {
+        var orderGenerator = new TeaOrderGenerator();
+        Order = orderGenerator.GenerateProduct(Number);
+        isOrderGenerated = true;
+        return Order;
+    }
+
+}
