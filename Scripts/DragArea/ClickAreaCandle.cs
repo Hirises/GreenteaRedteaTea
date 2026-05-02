7be3b9c980c03b554261d6e5eed9953ba62307cb
Hirises <hirises1243@gmail.com>
2026-05-02 12:21:01 +0900

    feat: 손님 입장음 추가

--- Scripts/DragArea/ClickAreaCandle.cs
+++ Scripts/DragArea/ClickAreaCandle.cs
@@ -3,66 +3,68 @@ using System;
 
 public partial class ClickAreaCandle : ClickArea
 {
-    [Export] float candleMaxTime = 3f;
-    [Export] DraggableTeapot teapot;
-    [Export] Node2D flameOn;
-    [Export] Node2D flameOff;
-    [Export] GpuParticles2D particle;
-    float candleTime = 0f;
-    bool isLit = false;
+	[Export] float candleMaxTime = 3f;
+	[Export] DraggableTeapot teapot;
+	[Export] Node2D flameOn;
+	[Export] Node2D flameOff;
+	[Export] GpuParticles2D particle;
+	float candleTime = 0f;
+	bool isLit = false;
 
-    public override void OnClick()
-    {
-        if (isLit)
-        {
-            GD.Print($"Candle {Name} is already lit.");
-            return;
-        }
-        GD.Print($"Lit candle {Name}!");
-        isLit = true;
-        candleTime = 0f;
-        SetFlameState(true);
-        SoundManager.Play(SFXType.CandleLit);
-    }
+	public override void OnClick()
+	{
+		if (isLit)
+		{
+			GD.Print($"Candle {Name} is already lit.");
+			return;
+		}
+		GD.Print($"Lit candle {Name}!");
+		isLit = true;
+		candleTime = 0f;
+		SetFlameState(true);
+		SoundManager.Play(SFXType.CandleLit);
+		SoundManager.Play(SFXType.TeapotBrewing);
+	}
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
 
-        if (isLit)
-        {
-            candleTime += (float)delta;
-            if (candleTime >= candleMaxTime)
-            {
-                isLit = false;
-                GD.Print($"Candle {Name} has burned out. Trying brew the tea in teapot...");
-                teapot.TryBrew();
-                SetFlameState(false);
-                return;
-            }
-            if (InputManager.Instance.currentDragItem == teapot)
-            {
-                isLit = false;
-                GD.Print($"Teapot is lifted while candle {Name} is lit. Extinguishing candle.");
-                SetFlameState(false);
-                return;
-            }
-        }
-    }
+		if (isLit)
+		{
+			candleTime += (float)delta;
+			if (candleTime >= candleMaxTime)
+			{
+				isLit = false;
+				GD.Print($"Candle {Name} has burned out. Trying brew the tea in teapot...");
+				teapot.TryBrew();
+				SetFlameState(false);
+				return;
+			}
+			if (InputManager.Instance.currentDragItem == teapot)
+			{
+				isLit = false;
+				SoundManager.Stop(SFXType.TeapotBrewing);
+				GD.Print($"Teapot is lifted while candle {Name} is lit. Extinguishing candle.");
+				SetFlameState(false);
+				return;
+			}
+		}
+	}
 
-    void SetFlameState(bool lit)
-    {
-        if (lit)
-        {
-            flameOff.Hide();
-            flameOn.Show();
-            particle.Emitting = true;
-        }
-        else
-        {
-            flameOff.Show();
-            flameOn.Hide();
-            particle.Emitting = false;
-        }
-    }
+	void SetFlameState(bool lit)
+	{
+		if (lit)
+		{
+			flameOff.Hide();
+			flameOn.Show();
+			particle.Emitting = true;
+		}
+		else
+		{
+			flameOff.Show();
+			flameOn.Hide();
+			particle.Emitting = false;
+		}
+	}
 }
